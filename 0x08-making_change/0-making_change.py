87ffa9849468ab1563c2292bfc06115ff5450ac8
Festongithub <Fkemboik@gmail.com>
2024-10-02 09:42:35 +0000

    Build

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,10 +2,21 @@
 
 """Coin changes"""
 
+
 def makeChange(coins, total):
     """
     ___summary___
     """
-    coins = []
-
-    i
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    change = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        temp = total // coin
+        change += temp
+        total -= (temp * coin)
+    if total != 0:
+        return -1
+    return change
